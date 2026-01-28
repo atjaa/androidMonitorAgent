774e0891e40f478a5c6d764711720b3be19f5493
jjzhuang83 <jjzhuang83@163.com>
2026-01-28 14:35:00 +0800

    增加远程抓取摄像头拍照

---
+++ app/src/main/java/com/atjaa/myapplication/service/ImagePreviewDialog.kt
@@ -0,0 +1,17 @@
+package com.atjaa.myapplication.service
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.widget.ImageView
+
+class ImagePreviewDialog(private val bitmap: Bitmap) : androidx.fragment.app.DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): android.app.Dialog {
+        val iv = ImageView(requireContext()).apply {
+            setImageBitmap(bitmap)
+        }
+        return android.app.AlertDialog.Builder(requireContext())
+            .setView(iv)
+            .setPositiveButton("关闭", null)
+            .create()
+    }
+}
